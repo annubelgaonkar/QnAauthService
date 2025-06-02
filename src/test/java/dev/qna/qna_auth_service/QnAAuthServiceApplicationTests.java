ea5b5427b666df6934636f7a83cf316e6a9f6446
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-06-02 14:35:59 +0530

    Modified application.properties for Remote DB

--- src/test/java/dev/qna/qna_auth_service/QnAAuthServiceApplicationTests.java
+++ src/test/java/dev/qna/qna_auth_service/QnAAuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package dev.qna.qna_auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QnAAuthServiceApplicationTests {
 
     @Test
